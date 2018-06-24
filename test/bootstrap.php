dc7e83339c3f25db445ba7d7847adce10180c9c2
Daniel Berthereau <Daniel.github@Berthereau.net>
2018-06-25 00:00:00 +0200

    Cleaned code.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 use OmekaTestHelper\Bootstrap;
 
@@ -8,5 +8,5 @@ Bootstrap::bootstrap(__DIR__);
 Bootstrap::loginAsAdmin();
 Bootstrap::enableModule('UpgradeFromOmekaClassic');
 
-require_once __DIR__ . '/../src/View/Helper/Upgrade.php';
-require_once __DIR__ . '/../src/View/Helper/Inflector.php';
+require_once dirname(__DIR__) . '/src/View/Helper/Upgrade.php';
+require_once dirname(__DIR__) . '/src/View/Helper/Inflector.php';
